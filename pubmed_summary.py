dcb7195404a00259ef46735a599850302753e6b9
shupengstyle <46952009+shupengstyle@users.noreply.github.com>
2025-02-03 15:01:43 +0800

    Update pubmed_summary.py

--- pubmed_summary.py
+++ pubmed_summary.py
@@ -48,9 +48,7 @@ def fetch_pubmed_articles(query="COVID-19", max_results=5):
         author_names = ', '.join([f"{author.find('lastname').text} {author.find('initials').text}" for author in authors if author.find('lastname')])
         
         # 提取期刊名称，使用大写的 <Journal> 标签
-        journal = soup.find("Journal")
-        iso_abbreviation = journal.find("ISOAbbreviation")
-        journal_name = iso_abbreviation.text.strip()
+        journal_name = soup.find("Journal").find("isoabbreviation").text if soup.find("Journal").find("isoabbreviation") else "无杂志名称"
         
         # 获取发表年份
         pub_date = soup.find("pubdate")
